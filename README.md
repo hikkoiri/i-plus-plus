6b249d4d17514adeeac45001e262aae624d6db11
Carlo Hildebrandt <github@carlo-hildebrandt.de>
2025-04-24 21:11:38 +0200

    implement svg endpoint

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
   - [About](#about)
   - [Working principles](#working-principles)
     - [Default header](#default-header)
-    - [`origin` global query parameters](#origin-global-query-parameters)
+    - [`origin` global query parameter](#origin-global-query-parameter)
   - [Endpoints](#endpoints)
     - [Standard JSON](#standard-json)
+    - [SVG](#svg)
+      - [Query Parameters](#query-parameters)
     - [Prometheus](#prometheus)
   - [Service deployment](#service-deployment)
     - [Deploment configuration](#deploment-configuration)
@@ -17,15 +19,19 @@
 ## About
 > With the deprecation of the popular repo <https://github.com/gjbae1212/hit-counter>, it is time to revive this project again.
 
-Count daily and overall website visits.
+Count daily and overall website visits:
+
+<a href="https://github.com/hikkoiri/i-plus-plus">
+    <img alt="Page visit counter" src="https://i-plus-plus.carlo-hildebrandt.de/svg" />
+</a>
 
 ## Working principles
-The way this service works is quite straight-forward. By calling this service the number of daily visits for the respective origin is increased by one. By the end of each day, the daily count will be reset and added to the total amount. There is an implicit and explicit way how the origin is identified. If none is identified it will default to `unknwown`.
+The way this service works is quite straight-forward. By calling this service the number of daily visits for the respective origin is increased by one. By the end of each day, the daily count will be reset and added to the total amount. There is an implicit and explicit way how the origin is identified. If none is identified it will default to `unknown`.
 
 ### Default header
 Implicitly the website, where this service can be embedded in, shall provide its hostname in the `origin` header that is read by the service.
 
-### `origin` global query parameters
+### `origin` global query parameter
 
 For other use cases, where you cannot append the origin http header or want to intentionally override it, you can append an explicit query parameter in the url path:
 ```
@@ -52,6 +58,37 @@ returns
 }
 ```
 
+### SVG
+
+Inspired by <https://github.com/hikkoiri/go-counter-badge> you may want to display a counter on your website.
+``` bash
+curl https://i-plus-plus.carlo-hildebrandt.de/
+```
+returns
+
+<svg xmlns="http://www.w3.org/2000/svg" width="241" height="20">
+    <rect x="5" width="156" height="20" fill="#333333" />
+    <rect width="161" height="20" fill="#333333" ry="5" rx="5"/>
+    <text x="10" y="14" fill="#fff" textAnchor="middle" font-family="Verdana" font-size="12">
+        hits (daily / all time)
+    </text>
+    <rect x="161" width="75" height="20" fill="darkgreen"/>
+    <rect x="161" width="80" height="20" fill="darkgreen" ry="5" rx="5" />
+    <text x="166" y="14" fill="#fff" textAnchor="middle" font-family="Verdana" font-size="12">
+        1 / 123456
+    </text>
+</svg>
+
+
+#### Query Parameters
+
+The following query parameters can be used to customize the SVG output:
+
+| Parameter              | Description                                                                           | Default Value |
+| ---------------------- | ------------------------------------------------------------------------------------- | ------------- |
+| `language`             | Sets the language for the left text. Supported values: `en` (English), `de` (German). | `en`          |
+| `leftBackgroundColor`  | Sets the background color of the left section of the SVG.                             | `"#333333"`   |
+| `rightBackgroundColor` | Sets the background color of the right section of the SVG.                            | `"darkgreen"` |
 
 ### Prometheus
 If you want to scrape the amount of users with prometheus, this is your endpoint. It will not bump the daily count, instead it only provides a read-only endpoint for the state that already matches the required Prometheus data model.
@@ -87,6 +124,6 @@ cdk destroy
 
 
 ## TODOs
-- [ ] Provide SVG Badge, like: <https://github.com/hikkoiri/go-counter-badge>
-- [ ]  Configure timezone for daily merge at midnight (right now UTC is used)
-- [ ]  Implement CI/CD pipeline
+- [ ] Make SVG sclaing more elegant 👀
+- [ ] Configure timezone for daily merge at midnight (right now UTC is used)
+- [ ] Implement CI/CD pipeline
--- infra/lib/lambdas/svg.ts
+++ infra/lib/lambdas/svg.ts
@@ -1,10 +1,141 @@
-import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+
 const COUNTER_TABLE_NAME = process.env.COUNTER_TABLE_NAME!;
 const COUNTER_PRIMARY_KEY = process.env.COUNTER_PRIMARY_KEY!;
 
 const db = new DynamoDBClient();
 
+
+interface Badge {
+    fontType: string;
+    leftText: string;
+    leftTextColor: string;
+    leftBackgroundColor: string;
+    rightText: string;
+    rightTextColor: string;
+    rightBackgroundColor: string;
+    radius: number;
+}
+
 export const handler = async (event: any = {}): Promise<any> => {
 
-    // TODO continue here
+    let origin;
+    const queryOrigin = event.queryStringParameters?.origin;
+    if (queryOrigin) {
+        console.log("Origin found in query parameters: " + queryOrigin);
+        origin = queryOrigin;
+    }
+    else {
+        origin = event.headers?.Origin || event.headers?.origin || "unknown";
+        console.log("Origin found from header" + origin)
+    }
+
+    var current = await getCurrentCount(origin)
+    console.log("Retrieved record from database to: " + JSON.stringify(current))
+
+    var bumped = {
+        ...current,
+        dailyCount: current.dailyCount + 1,
+    }
+    console.log("Updating value in database to: " + JSON.stringify(bumped))
+    await updateCount(bumped)
+
+
+
+
+
+    // now check the query parameter for visual configuration
+    const language = event.queryStringParameters?.language || "en";
+    let leftText = event.queryStringParameters?.leftText;
+    if (!leftText) {
+        leftText = language === "de" ? "Aufrufe (täglich / insgesamt)" : "hits (daily / all time)";
+    }
+    const leftBackgroundColor = event.queryStringParameters?.leftBackgroundColor || "#333333"
+    const rightBackgroundColor = event.queryStringParameters?.rightBackgroundColor || "darkgreen"
+
+
+    const rightText = `${bumped.dailyCount} / ${bumped.allTimeCount}`;
+    const badge: Badge = {
+        fontType: "Verdana",
+        rightTextColor: "#fff",
+        leftTextColor: "#fff",
+        leftText,
+        leftBackgroundColor,
+        rightText,
+        rightBackgroundColor,
+        radius: 5,
+    };
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": origin,
+            "Content-Type": "image/svg+xml"
+        },
+        body: renderFlatBadge(badge)
+    };
+
+}
+
+
+function renderFlatBadge(badge: Badge): string {
+
+    const defaultBadgeHeight = 20;
+    const defaultFontSize = 12;
+    const leftWidth = badge.leftText.length * 7; // Approximation for text width
+    const rightWidth = badge.rightText.length * 8; // Approximation for text width
+
+    return `
+    <svg xmlns="http://www.w3.org/2000/svg" width="${leftWidth + rightWidth}" height="${defaultBadgeHeight}">
+        <rect x="${badge.radius}" width="${leftWidth - badge.radius}" height="${defaultBadgeHeight}" fill="${badge.leftBackgroundColor}" />
+        <rect width="${leftWidth}" height="${defaultBadgeHeight}" fill="${badge.leftBackgroundColor}" ry="${badge.radius}" rx="${badge.radius}"/>
+        <text x="${2 * badge.radius}" y="${14}" fill="${badge.leftTextColor}" textAnchor="middle" font-family="${badge.fontType}" font-size="${defaultFontSize}">
+            ${badge.leftText}
+        </text>
+        <rect x="${leftWidth}" width="${rightWidth - badge.radius}" height="${defaultBadgeHeight}" fill="${badge.rightBackgroundColor}"/>
+        <rect x="${leftWidth}" width="${rightWidth}" height="${defaultBadgeHeight}" fill="${badge.rightBackgroundColor}" ry="${badge.radius}" rx="${badge.radius}" />
+        <text x="${leftWidth + badge.radius}" y="${14}" fill="${badge.rightTextColor}" textAnchor="middle" font-family="${badge.fontType}" font-size="${defaultFontSize}">
+            ${badge.rightText}
+        </text>
+    </svg>`
+}
+
+
+async function getCurrentCount(origin: string): Promise<any> {
+    const params = {
+        TableName: COUNTER_TABLE_NAME,
+        Key: {
+            [COUNTER_PRIMARY_KEY]: { S: origin }
+        }
+    };
+    const command = new GetItemCommand(params);
+    const response = await db.send(command);
+    if (!response.Item) {
+        return {
+            origin,
+            allTimeCount: 0,
+            dailyCount: 0
+        };
+    }
+    else {
+        return {
+            origin,
+            allTimeCount: parseInt(response.Item.allTimeCount.N!),
+            dailyCount: parseInt(response.Item.dailyCount.N!)
+        };
+    }
+}
+
+
+async function updateCount(bumped: any): Promise<void> {
+    const params = {
+        TableName: COUNTER_TABLE_NAME,
+        Item: {
+            [COUNTER_PRIMARY_KEY]: { S: bumped.origin },
+            allTimeCount: { N: bumped.allTimeCount.toString() },
+            dailyCount: { N: bumped.dailyCount.toString() }
+        }
+    };
+    const command = new PutItemCommand(params);
+    await db.send(command);
 }
