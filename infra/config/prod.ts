84f359522667f5ced797343f9c0a7f8fdeb6e8e7
Carlo Hildebrandt <github@carlo-hildebrandt.de>
2025-04-24 21:51:22 +0200

    \n

--- infra/config/prod.ts
+++ infra/config/prod.ts
@@ -4,5 +4,5 @@ export const prodProps: IPlusPlusStackProps = {
     domainName: 'carlo-hildebrandt.de',
     subdomainName: 'i-plus-plus',
     corsAllowedOrigin: '*',
-    existingWildCardDomainCertArn: 'arn:aws:acm:eu-central-1:xxx:certificate/yyy'
+    existingWildCardDomainCertArn: 'arn:aws:acm:eu-central-1:866299738543:certificate/26e193ed-a654-4918-b3dd-ed06493d906b'
 }
